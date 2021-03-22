Fixed lacking gradient bug and actually use the new layer

rl/layers/nmlinear.py
@@ -5,20 +5,20 @@ import torch.nn.functional as F
 class NMLinear(nn.Module):
 	def __init__(self, input_channels, output_channels):
 		super(NMLinear, self).__init__()
-		self.weight = nn.Parameter(torch.Tensor(input_channels, output_channels), requires_grad=True)
+		# self.weight = nn.Parameter(torch.Tensor(input_channels, output_channels), requires_grad=True)
 
-		self.ws = nn.Linear(input_channels, 8) # Hardcode 8 for now, TODO: Make this a parameter in constructor this
-		self.wm = nn.Linear(8, 8)
+		self.ws = nn.Linear(input_channels, output_channels) # Hardcode 8 for now, TODO: Make this a parameter in constructor this
+		self.wm = nn.Linear(input_channels, 8)
 		self.wn = nn.Linear(8, output_channels)
 
-		self.bias = nn.Parameter(torch.Tensor(output_channels), requires_grad=True)
-
+		# self.bias = nn.Parameter(torch.Tensor(output_channels), requires_grad=True)
 
 	def forward(self, input):
-		y = F.ReLU(self.wm(input))
-		y_prime = F.tanh(self.wn(y))
+
+		y = F.relu(self.wm(input))
+		y_prime = self.wn(y).tanh() # F.tanh is deprecated, source code for F.tanh(input) just calls input.tanh
 
 		z = self.ws(input)
 
-		output = F.ReLu(z * y_prime)
+		output = F.relu(z * y_prime)
 		return output

rl/policies/normal_mlp.py
@@ -77,15 +77,11 @@ class CaviaMLPPolicy(Policy, nn.Module):
 
         layer_sizes = (input_size,) + hidden_sizes
 
-        for i in range(self.num_layers):
-            print("DEBUG")
-            print(layer_sizes[i])
-
         self.add_module('layer{0}'.format(1), nn.Linear(layer_sizes[0] + num_context_params, layer_sizes[1]))
         self.add_module('nm_layer{0}'.format(1), layers.NMLinear(layer_sizes[1], layer_sizes[1]))
         for i in range(2, self.num_layers):
             self.add_module('layer{0}'.format(i), nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
-            self.add_module('layer{0}'.format(i), layers.NMLinear(layer_sizes[i], layer_sizes[i]))
+            self.add_module('nm_layer{0}'.format(i), layers.NMLinear(layer_sizes[i], layer_sizes[i]))
 
         self.num_context_params = num_context_params
         self.context_params = torch.zeros(self.num_context_params, requires_grad=True).to(self.device)
@@ -101,6 +97,13 @@ class CaviaMLPPolicy(Policy, nn.Module):
         if params is None:
             params = OrderedDict(self.named_parameters())
 
+
+        # TODO: There must be a better way of doing this instead of getting each layer every loop
+        #       We just want to access the nm_layers to send it through
+        layers = OrderedDict()
+        for name, module in self.named_modules():
+            layers[name] = module
+
         # concatenate context parameters to input
         output = torch.cat((input, self.context_params.expand(input.shape[:-1] + self.context_params.shape)),
                            dim=len(input.shape) - 1)
@@ -112,9 +115,8 @@ class CaviaMLPPolicy(Policy, nn.Module):
             output = self.nonlinearity(output)
 
             # forward through NeuroModulator layers
-            if i == 0:
-                output = F.linear(output, weight=params['nm_layer{0}.weight'.format(i)],
-                              bias=params['nm_layer{0}.bias'.format(i)])
+            nm_layer = layers["nm_layer{0}".format(i)]
+            output = nm_layer(output)
 
         # last layer outputs mean; scale is a learned param independent of the input
         mu = F.linear(output, weight=params['mu.weight'], bias=params['mu.bias'])